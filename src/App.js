521df1761195fc72b79fdd685ad62ff59cca0e51
Renato <renato@Renatos-iMac.local>
2020-04-30 19:02:25 -0300

    Portalzs em NodeJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Routes from "./routes";
+
+import "./AdminLTE.css";
+import "./bower_components/bootstrap/dist/css/bootstrap.min.css"
+import "./bower_components/font-awesome/css/font-awesome.min.css"
+import "./bower_components/Ionicons/css/ionicons.min.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
---
+++ src/pages/Logon/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+import api from '../../services/api'
+import santanderLogo from '../../assets/santander-logo-2.png';
+import loadingGif from '../../assets/login_loading.gif';
+
+import './styles.css';
+
+export default function Logon() {
+
+  const [hiddenLogin, setHiddenLogin] = useState(true);
+  const [hiddenAlert, setHiddenAlert] = useState(true);
+  const [message, setMessage] = useState('');
+  const [sinistro, setSinistro] = useState('2020-71-29434-0');
+
+  async function LoginSinistro(){
+
+      setHiddenLogin(false);
+      if (!sinistro) {
+          setHiddenLogin(true);
+          setHiddenAlert(false);
+          setMessage(' Digite o nº de sinistro');
+          return false;
+      }
+    
+      api.get(`login/sinistro/${sinistro}`)
+        .then(response =>{
+
+          const { status } = response.data;  
+            
+          if (!status) {
+              setHiddenLogin(true);
+              setHiddenAlert(false);
+              setMessage(' Sinistro não localizado');
+          }
+
+          console.log(response.data);
+            
+      })
+
+  }
+
+
+  return (
+  <div>
+      <div className="login-box">
+            <div className="login-logo">
+            <img src={santanderLogo} alt="Logo Santander" className="logo"/>
+            </div>
+
+        <div className="login-box-body">
+            <p className="login-box-msg"></p>
+            <div className="form-group has-feedback">
+                <input value={sinistro} onChange={e => setSinistro(e.target.value)} type="text" className="form-control" placeholder="Digite o Nº Sinistro" maxLength="35" style={{ fontWeight: 'bold' }} />
+            </div>
+
+            <div className="row">
+                <div className="col-xs-8">
+                    <div className="checkbox icheck">
+                        <label>
+                        </label>
+                    </div>
+                </div>
+            
+                <div className="col-xs-4">
+                <button onClick={LoginSinistro} className="btn btn-primary btn-block btn-flat" style={{ fontFamily: "Open Sans,sans-serif" }} >Confirmar</button>
+            </div>
+        </div>
+        </div>
+        <div className="alert alert-danger line_danger" role="alert" hidden={hiddenAlert}>
+            <span className="glyphicon glyphicon-info-sign"></span><span>{message}</span>
+        </div>
+      </div>
+      <div id="loading-login" className="loading" hidden={hiddenLogin}>
+        <img src={loadingGif} alt="Loading" />
+      </div>
+  </div>
+  );
+}
