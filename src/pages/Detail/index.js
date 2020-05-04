7ba2bee673638adc6f14d1bed8f959803852b800
Renato <renato@Renatos-iMac.local>
2020-05-04 12:14:12 -0300

    Rebuild no Sass Docker e Implantação de Sass

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,186 +1,233 @@
 import React, { useEffect, useState } from "react";
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
-import LogoUser from '../../assets/user2-.jpg';
-import SantanderLogo from '../../assets/santander-logo-2.png';
-import './styles.css';
+import LogoUser from "../../assets/user2-.jpg";
+import SantanderLogo from "../../assets/santander-logo-2.png";
 import api from "../../services/api";
 
-
-
-
 export default function Detail() {
+  const history = useHistory();
+  const status = localStorage.getItem("status");
+  const [nomeSegurado, setNomeSegurado] = useState("");
+  const [nomeSinistrado, setNomeSinistrado] = useState("");
+  const [nomeNotificante, setNomeNotificante] = useState("");
+  const [cpfSegurado, setCpfSegurado] = useState("");
+  const [cpfSinistrado, setCpfSinistrado] = useState("");
+  const [cpfNotificante, setCpfNotificante] = useState("");
+  const [numSinistro, setNumSinistro] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [cobertura, setCobertura] = useState("");
+  const [apolice, setApolice] = useState("");
 
-    const history = useHistory();
-    const status = localStorage.getItem('status');
-    const [nomeSegurado, setNomeSegurado] = useState('');
-    const [nomeSinistrado, setNomeSinistrado] = useState('');
-    const [nomeNotificante, setNomeNotificante] = useState('');
-    const [cpfSegurado, setCpfSegurado] = useState('');
-    const [cpfSinistrado, setCpfSinistrado] = useState('');
-    const [cpfNotificante, setCpfNotificante] = useState('');
-    const [numSinistro, setNumSinistro] = useState('');
-    const [email, setEmail] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [cobertura, setCobertura] = useState('');
-    const [apolice, setApolice] = useState('');
+  if (!status) history.push("/");
 
-    if (!status) history.push('/');
-
-
-    //DropMenu status bar
-    window.onclick = function(event) {
-        
-        if (!event.target.matches('.hidden-xs')) {
-            let dropdowns = document.getElementsByClassName("dropmenu");  
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-                }
-            }
+  //DropMenu status bar
+  window.onclick = function (event) {
+    if (!event.target.matches(".hidden-xs")) {
+      let dropdowns = document.getElementsByClassName("dropmenu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
         }
-
-        
+      }
     }
+  };
 
-    useEffect(() => { 
-        const record = localStorage.getItem('record');
-        SinistroDocumento(record);
-    }, []);
+  useEffect(() => {
+    const record = localStorage.getItem("record");
+    SinistroDocumento(record);
+  }, []);
 
+  async function SinistroDocumento(props) {
+    await api.get(`documento/${props}`, {}).then((response) => {
+      const data = response.data;
+      // console.log(data[0]);
+      setNomeSegurado(data[0].nome_segurado);
+      setNomeSinistrado(data[0].nome_sinistrado);
+      setNomeNotificante(data[0].nome_notificante);
+      setCpfSegurado(data[0].cpf_segurado);
+      setCpfSinistrado(data[0].cpf_sinistrado);
+      setCpfNotificante(data[0].cpf_notificante);
+      setNumSinistro(data[0].num_sinistro);
+      setEmail(data[0].email_notificante);
+      setTelefone(data[0].telefone);
+      setCobertura(data[0].cobertura_reclamada);
+      setApolice(data[0].apolice);
+    });
+  }
 
-    async function SinistroDocumento(props) {
+  function handleExit() {
+    localStorage.clear();
+    history.push("/");
+  }
 
-        await api.get(`documento/${props}`, {
+  function DropMenu(e) {
+    e.preventDefault();
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
 
-        }).then(response => {
-            const data = response.data;
-            // console.log(data[0]);
-            setNomeSegurado(data[0].nome_segurado);
-            setNomeSinistrado(data[0].nome_sinistrado);
-            setNomeNotificante(data[0].nome_notificante);
-            setCpfSegurado(data[0].cpf_segurado);
-            setCpfSinistrado(data[0].cpf_sinistrado);
-            setCpfNotificante(data[0].cpf_notificante);
-            setNumSinistro(data[0].num_sinistro);
-            setEmail(data[0].email_notificante);
-            setTelefone(data[0].telefone);
-            setCobertura(data[0].cobertura_reclamada);
-            setApolice(data[0].apolice);
-        })
-    }
-
-    function handleExit() {
-        localStorage.clear();
-        history.push('/');
-    }
-
-    function DropMenu(e) {
-        e.preventDefault();
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-
-
-    return(
-        <div>
-            <header className="top">
-                <a href="#" onClick={DropMenu}>
-                    <img src={LogoUser} className="user-image" alt="User Image"/>
-                    <span id="nome_cliente" className="hidden-xs">LUCAS GONCALVES DE ARAUJO ROBERTO</span>
-                </a>
-            </header>
-            <div className="dropmenu" id="myDropdown">
-                <div className="dropmenu-btn">
-                    <img src={LogoUser} className="image-drop" alt="Image DropMenu"/>
-                    <span className="hidden-large" >LUCAS GONCALVES DE ARAUJO ROBERTO</span>
-                </div>
-                <div className="pull-right btn-detail">
-                    <button onClick={handleExit}  className="btn btn-default btn-flat">Sair</button>
-                </div>
-            </div>
-
-            <h1>
-                <span className="logo-lg">
-                    <img src={SantanderLogo} alt="Santander Logo"/>
-                </span>
-            </h1>
-
-            <h2 className="h2-size">
-                Bem vindo ao Portal de Sinistros Santander.
-            </h2>
-            <h4 className="h4-size">
-                O que a gente pode fazer por você hoje?
-            </h4>
-
-            {/*FORMULARIO DE DADOS DO CLIENTES*/}
-
-            <div className="row-detail">
-                <div className="form-group col-md-4">
-                    <label >Nome do Segurado</label>
-                    <input type="text" value={nomeSegurado} className="form-control" readOnly={true}/>
-                </div>
-            <div className="form-group col-md-4">
-                    <label >Nome do Sinistrado</label>
-                    <input type="text" value={nomeSinistrado} className="form-control" readOnly={true}/>
-                </div>
-            <div className="form-group col-md-4">
-                    <label >Nome do Notificante</label>
-                    <input type="text" value={nomeNotificante} className="form-control" readOnly={true}/>
-                </div>
-            </div>
-
-            <div className="row-detail">
-                <div className="form-group col-md-3">
-                    <label>CPF Segurado</label>
-                    <input type="text" value={cpfSegurado} className="form-control" readOnly={true}/>
-                </div>
-            <div className="form-group col-md-3">
-                    <label>CPF Sinistrado</label>
-                    <input type="text" value={cpfSinistrado} className="form-control" readOnly={true}/>
-                </div>
-            <div className="form-group col-md-3">
-                    <label>CPF Notificante</label>
-                    <input type="text" value={cpfNotificante} className="form-control" readOnly={true}/>
-                </div>
-                <div className="form-group col-md-3">
-                    <label>Nº Sinistro</label>
-                    <input type="text" value={numSinistro} className="form-control" readOnly={true}/>
-                </div>
-            </div>
-
-            <div className="row-detail">
-                <div className="form-group col-md-3">
-                    <label>E-mail <span style={{ color: 'red', fontSize: 14 }}>*</span></label>
-                    <input type="email" value={email} className="form-control" readOnly={true}/>
-                </div>
-                <div className="form-group col-md-3">
-                    <label>Telefone Contato <span style={{ color: 'red', fontSize: 14 }}>*</span></label>
-                    <input type="text" value={telefone} className="form-control" readOnly={true}/>
-                </div>
-                <div className="form-group col-md-3">
-                        <label>Cobertura Reclamada</label>
-                        <input type="text" value={cobertura} className="form-control" readOnly={true}/>
-                </div>
-                <div className="form-group col-md-3">
-                    <label>Apolice</label>
-                    <input type="text" value={apolice} className="form-control" readOnly={true}/>
-                </div>
-            </div>
-
-            <ul className="nav nav-tabs menu-bar">       
-                <li id="bar-status">
-                    <a href="javascript:TabMenu('status')">Status do Sinistro</a>
-                </li>         
-                <li className="active" id="bar-doc">
-                    <a href="#">Solicitação de Documentos</a>
-                </li>
-            </ul>
-            <div class="callout-bar documentos" >
-                <h4 class="h4-font-size">DOCUMENTOS SOLICITADO PELA SEGURADORA</h4>
-            </div>
-        
+  return (
+    <div>
+      <header className="top">
+        <a href="#" onClick={DropMenu}>
+          <img src={LogoUser} className="user-image" alt="User Image" />
+          <span id="nome_cliente" className="hidden-xs">
+            LUCAS GONCALVES DE ARAUJO ROBERTO
+          </span>
+        </a>
+      </header>
+      <div className="dropmenu" id="myDropdown">
+        <div className="dropmenu-btn">
+          <img src={LogoUser} className="image-drop" alt="Image DropMenu" />
+          <span className="hidden-large">
+            LUCAS GONCALVES DE ARAUJO ROBERTO
+          </span>
         </div>
-    );
+        <div className="pull-right btn-detail">
+          <button onClick={handleExit} className="btn btn-default btn-flat">
+            Sair
+          </button>
+        </div>
+      </div>
+
+      <h1>
+        <span className="logo-lg">
+          <img src={SantanderLogo} alt="Santander Logo" />
+        </span>
+      </h1>
+
+      <h2 className="h2-size">Bem vindo ao Portal de Sinistros Santander.</h2>
+      <h4 className="h4-size">O que a gente pode fazer por você hoje?</h4>
+
+      {/*FORMULARIO DE DADOS DO CLIENTES*/}
+
+      <div className="row-detail">
+        <div className="form-group col-md-4">
+          <label>Nome do Segurado</label>
+          <input
+            type="text"
+            value={nomeSegurado}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-4">
+          <label>Nome do Sinistrado</label>
+          <input
+            type="text"
+            value={nomeSinistrado}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-4">
+          <label>Nome do Notificante</label>
+          <input
+            type="text"
+            value={nomeNotificante}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+      </div>
+
+      <div className="row-detail">
+        <div className="form-group col-md-3">
+          <label>CPF Segurado</label>
+          <input
+            type="text"
+            value={cpfSegurado}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-3">
+          <label>CPF Sinistrado</label>
+          <input
+            type="text"
+            value={cpfSinistrado}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-3">
+          <label>CPF Notificante</label>
+          <input
+            type="text"
+            value={cpfNotificante}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-3">
+          <label>Nº Sinistro</label>
+          <input
+            type="text"
+            value={numSinistro}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+      </div>
+
+      <div className="row-detail">
+        <div className="form-group col-md-3">
+          <label>
+            E-mail <span style={{ color: "red", fontSize: 14 }}>*</span>
+          </label>
+          <input
+            type="email"
+            value={email}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-3">
+          <label>
+            Telefone Contato{" "}
+            <span style={{ color: "red", fontSize: 14 }}>*</span>
+          </label>
+          <input
+            type="text"
+            value={telefone}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-3">
+          <label>Cobertura Reclamada</label>
+          <input
+            type="text"
+            value={cobertura}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group col-md-3">
+          <label>Apolice</label>
+          <input
+            type="text"
+            value={apolice}
+            className="form-control"
+            readOnly={true}
+          />
+        </div>
+      </div>
+
+      <ul className="nav nav-tabs menu-bar">
+        <li id="bar-status">
+          <a href="javascript:TabMenu('status')">Status do Sinistro</a>
+        </li>
+        <li className="active" id="bar-doc">
+          <a href="#">Solicitação de Documentos</a>
+        </li>
+      </ul>
+      <div class="callout-bar documentos">
+        <h4 class="h4-font-size">DOCUMENTOS SOLICITADO PELA SEGURADORA</h4>
+      </div>
+    </div>
+  );
 }
--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,94 +1,104 @@
 import React, { useState } from "react";
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
-import api from '../../services/api'
-import santanderLogo from '../../assets/santander-logo-2.png';
-import loadingGif from '../../assets/login_loading.gif';
-
-import './styles.css';
+import api from "../../services/api";
+import santanderLogo from "../../assets/santander-logo-2.png";
+import loadingGif from "../../assets/login_loading.gif";
 
 export default function Logon() {
-
   const history = useHistory();
 
   const [hiddenLogin, setHiddenLogin] = useState(true);
   const [hiddenAlert, setHiddenAlert] = useState(true);
-  const [message, setMessage] = useState('');
-  const [sinistro, setSinistro] = useState('2020-71-29434-0');
+  const [message, setMessage] = useState("");
+  const [sinistro, setSinistro] = useState("2020-71-29434-0");
 
-  const status = localStorage.getItem('status');
+  const status = localStorage.getItem("status");
 
-    if (status) history.push('/portal');
+  if (status) history.push("/portal");
 
-  async function LoginSinistro(e){
-      e.preventDefault();
+  async function LoginSinistro(e) {
+    e.preventDefault();
 
-      setHiddenLogin(false);
-      if (!sinistro) {
-          setHiddenLogin(true);
-          setHiddenAlert(false);
-          setMessage(' Digite o nº de sinistro');
-          return false;
+    setHiddenLogin(false);
+    if (!sinistro) {
+      setHiddenLogin(true);
+      setHiddenAlert(false);
+      setMessage(" Digite o nº de sinistro");
+      return false;
+    }
+
+    await api.get(`login/sinistro/${sinistro}`).then((response) => {
+      const { status } = response.data;
+
+      if (!status) {
+        setHiddenLogin(true);
+        setHiddenAlert(false);
+        setMessage(" Sinistro não localizado");
       }
-    
-      await api.get(`login/sinistro/${sinistro}`)
-        .then(response =>{
 
-          const { status } = response.data;  
-            
-          if (!status) {
-              setHiddenLogin(true);
-              setHiddenAlert(false);
-              setMessage(' Sinistro não localizado');
-          }
+      const { email, nome, ramo, record } = response.data;
+      localStorage.setItem("email", email);
+      localStorage.setItem("nome", nome);
+      localStorage.setItem("ramo", ramo);
+      localStorage.setItem("record", record);
+      localStorage.setItem("status", status);
 
-          const { email, nome, ramo, record } = response.data;
-          localStorage.setItem('email', email);
-          localStorage.setItem('nome', nome);
-          localStorage.setItem('ramo', ramo);
-          localStorage.setItem('record', record);
-          localStorage.setItem('status', status);
-
-          history.push("/confirma");
-            
-      })
-
+      history.push("/confirma");
+    });
   }
 
-
   return (
-  <div>
+    <div>
       <div className="login-box">
-            <div className="login-logo">
-            <img src={santanderLogo} alt="Logo Santander" className="logo"/>
-            </div>
+        <div className="login-logo">
+          <img src={santanderLogo} alt="Logo Santander" className="logo" />
+        </div>
 
         <div className="login-box-body">
-            <p className="login-box-msg"></p>
-            <div className="form-group has-feedback">
-                <input value={sinistro} onChange={e => setSinistro(e.target.value)} type="text" className="form-control" placeholder="Digite o Nº Sinistro" maxLength="35" style={{ fontWeight: 'bold' }} />
+          <p className="login-box-msg"></p>
+          <div className="form-group has-feedback">
+            <input
+              value={sinistro}
+              onChange={(e) => setSinistro(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="Digite o Nº Sinistro"
+              maxLength="35"
+              style={{ fontWeight: "bold" }}
+            />
+          </div>
+
+          <div className="row">
+            <div className="col-xs-8">
+              <div className="checkbox icheck">
+                <label></label>
+              </div>
             </div>
 
-            <div className="row">
-                <div className="col-xs-8">
-                    <div className="checkbox icheck">
-                        <label>
-                        </label>
-                    </div>
-                </div>
-            
-                <div className="col-xs-4">
-                <button onClick={LoginSinistro} className="btn btn-primary btn-block btn-flat" style={{ fontFamily: "Open Sans,sans-serif" }} >Confirmar</button>
+            <div className="col-xs-4">
+              <button
+                onClick={LoginSinistro}
+                className="btn btn-primary btn-block btn-flat"
+                style={{ fontFamily: "Open Sans,sans-serif" }}
+              >
+                Confirmar
+              </button>
             </div>
+          </div>
         </div>
-        </div>
-        <div className="alert alert-danger line_danger" role="alert" hidden={hiddenAlert}>
-            <span className="glyphicon glyphicon-info-sign"></span><span>{message}</span>
+        <div
+          className="alert alert-danger line_danger"
+          role="alert"
+          hidden={hiddenAlert}
+        >
+          <span className="glyphicon glyphicon-info-sign"></span>
+          <span>{message}</span>
         </div>
       </div>
       <div id="loading-login" className="loading" hidden={hiddenLogin}>
         <img src={loadingGif} alt="Loading" />
       </div>
-  </div>
+    </div>
   );
 }
